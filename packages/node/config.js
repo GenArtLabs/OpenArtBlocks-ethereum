8cac4519d12380a3d9aa6cec832836fb7de3e1b0
Louis MOUHAT <louis.mouhat@gmail.com>
2021-10-19 00:30:16 +0200

    switch main start to api

--- packages/node/config.js
+++ packages/node/config.js
@@ -4,7 +4,7 @@ const config = require("./hardhat_contracts.json");
 const contract = Object.values(Object.values(Object.values(config)[0])[0].contracts)[0];
 
 // Change the web3 provider here (local node / infura...)
-const PROVIDER_ADDRESS = "http://localhost:8545";
+const PROVIDER_ADDRESS = process.env.PROVIDER_ADDRESS ?? "http://localhost:8545";
 
 const { abi: ABI, address: CONTRACT_ADDRESS } = contract;
 
